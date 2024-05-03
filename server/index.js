f649a49ec2fb416d9889221ddc119fbd74aef0ab
dung2012 <dungchuhung@gmail.com>
2024-05-04 00:15:55 +0700

    v2

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('dotenv').config()
 const cors = require('cors')
+const dbconn = require('./config/dbconn')
 const app = express()
 app.use(
     cors({
@@ -10,6 +11,9 @@ app.use(
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+dbconn()
+
 const PORT = process.env.PORT || 7777
 
 app.listen(PORT, () => console.log(':::::::SERVER RUNNING ON PORT 5000:::::::'))
+// Path: server/index.js
